ec9a0f9cb7df207a4011b61580f66a0754fd522f
Louis Ian <louisiangt@gmail.com>
2021-05-23 23:57:46 -0300

    App Color scheme

---
+++ doge_finances/lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Main/Dark theme
+const dogeBlack = const Color.fromRGBO(0, 0, 0, 1);
+const dogeMidnight = const Color.fromRGBO(22, 22, 44, 1);
+const dogeCloudy = const Color.fromRGBO(61, 60, 85, 1);
+const dogeLilac = const Color.fromRGBO(172, 172, 255, 1);
+const dogeWhite = const Color.fromRGBO(255, 255, 255, 1);
