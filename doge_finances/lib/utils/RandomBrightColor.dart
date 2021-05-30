037ad7b180993cc6b4d5c68f2f1047ebfe72b065
Louis Ian <louisiangt@gmail.com>
2021-05-30 04:55:27 -0300

    BrightColor is now a class

---
+++ doge_finances/lib/utils/RandomBrightColor.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+import 'dart:ui';
+
+class RandomBrightColor{
+  Color generate(){
+    var color69 = new Random()
+        .nextInt(3); // Choose which field (R, G, or B) will have the 69 value
+    var color222 = new Random()
+        .nextBool(); // Choose which field the 2 fields that are left (the first or the second) will have the 222 value
+    var colorVal = 69 + new Random().nextInt(153); // Choose tha random value for the last field
+
+    var finalColor;
+
+    if (color69 == 0) {
+      if (color222) {
+        finalColor = Color.fromRGBO(69, 222, colorVal, 1);
+      } else {
+        finalColor = Color.fromRGBO(69, colorVal, 222, 1);
+      }
+    } else if (color69 == 1) {
+      if (color222) {
+        finalColor = Color.fromRGBO(222, 69, colorVal, 1);
+      } else {
+        finalColor = Color.fromRGBO(colorVal, 69, 222, 1);
+      }
+    } else {
+      if (color222) {
+        finalColor = Color.fromRGBO(222, colorVal, 69, 1);
+      } else {
+        finalColor = Color.fromRGBO(colorVal, 222, 69, 1);
+      }
+    }
+
+    return finalColor;
+  }
+}
