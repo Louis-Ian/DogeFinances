d5ee88590c3c7c9a608ba522cc1b20eb04fd6674
Louis-Ian <louisiangt@gmail.com>
2022-06-05 02:39:07 -0300

    Add account test 2: account with some expenses

--- doge_finances/test/domain/usecases/add_account_test.dart
+++ doge_finances/test/domain/usecases/add_account_test.dart
@@ -1,3 +1,5 @@
+import 'package:doge_finances/app/doge_finances/domain/entities/transfer.dart';
+import 'package:doge_finances/app/doge_finances/domain/usecases/delete_account.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -11,27 +13,62 @@ import 'add_account_test.mocks.dart';
 
 @GenerateMocks([AccountsRepository])
 void main() {
-  AddAccount usecase;
   MockAccountsRepository repository;
 
   group(
-    'Add Account',
+    'Add account',
     () {
       const accColor = Colors.white;
       repository = MockAccountsRepository();
-      usecase = AddAccount(repository);
+      AddAccount usecase = AddAccount(repository);
+
       test(
         'Add empty account',
         () async {
           // Arrange
           Account emptyAccount = Account(0, accColor, []);
+
           when(repository.addAccount(any))
               .thenAnswer((_) async => Right(emptyAccount.id as int));
+
           // Act
           final result = await usecase.execute(account: emptyAccount);
+
           // Assert
           expect(result, Right(emptyAccount.id as int));
           verify(repository.addAccount(emptyAccount));
+          verifyNoMoreInteractions(repository);
+        },
+      );
+
+      test(
+        'Add account with some expenses registered',
+        () async {
+          // Arrange
+          Account testAccount = Account(200, accColor, []);
+          testAccount.addPayment(Transfer(
+              value: 20,
+              originAccountID: testAccount,
+              destinationAccountID: null,
+              description: 'test expense 1',
+              date: DateTime(2022, 04, 20)));
+
+          testAccount.addPayment(Transfer(
+              value: 40,
+              originAccountID: testAccount,
+              destinationAccountID: null,
+              description: 'test expense 2',
+              date: DateTime(2022, 05, 21)));
+
+          when(repository.addAccount(any))
+              .thenAnswer((_) async => Right(testAccount.id as int));
+
+          // Act
+          final result = await usecase.execute(account: testAccount);
+
+          // Assert
+          expect(result, Right(testAccount.id as int));
+          verify(repository.addAccount(testAccount));
           verifyNoMoreInteractions(repository);
         },
       );
